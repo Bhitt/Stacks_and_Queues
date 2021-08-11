fedc43ba49a3481184903b1b1927ab40bc9501e7
Bhitt <BrandenHitt@gmail.com>
2021-08-11 13:58:22 -0700

    Three in One problem and solution

---
+++ Three_in_One/Solution1.java
@@ -0,0 +1,76 @@
+/* 
+	Solution 1 
+	
+	Fixed division approach.
+	Divide the array into three equal parts and allow the stack to grow in that limited space.
+*/
+
+
+/* 
+	Time Complexity: 
+	Space Complexity: 
+*/
+
+class FixedMultiStack {
+	private int numberOfStacks = 3;
+	private int stackCapacity;
+	private int[] values;
+	private int[] sizes;
+
+	public FixedMultiStack(int stackSize){
+		stackCapacity = stackSize;
+		values = new int[stackSize * numberOfStacks)];
+		sizes = new int[numberOfStacks];
+	}
+
+	/* push value onto the stack */
+	public void push(int stackNum, int value) throws FullStackException{
+		/* check that we have space for the element */
+		if(isFull(stackNum)){
+			throw new FullStackException();
+		}
+
+		/* increment stack pointer and then update top value */
+		sizes[stackNum]++;
+		values[indexOfTop(stackNum)] = value;
+	}
+
+	/* pop item from the stack */
+	public int pop(int stackNum){
+		if(isEmpty(stackNum)){
+			throw new EmptyStackException;
+		}
+
+		int topIndex = indexOfTop(stackNum);
+		int value = values[topIndex]; //Get top
+		values[topIndex] = 0; //clear
+		sizes[stackNum]--; //shrink
+		return value;
+	}
+
+	/* return top element */
+	public int peek(int stackNum){
+		if(isEmpty(stackNum)){
+			throw new EmptyStackException;
+		}
+
+		return values[indexOfTop(stackNum)]
+	}
+
+	/* return if stack is empty */
+	public boolean isEmpty(int stackNum){
+		return sizes[stackNum] == 0;
+	}
+
+	/* return if stack is full */
+	public boolean isFull(int stackNum){
+		return sizes[stackNum] == stackCapacity;
+	}
+
+	/* returns index of the top of the stack */
+	private int indexOfTop(int stackNum){
+		int offset = stackNum * stackCapacity;
+		int size = sizes[stackNum];
+		return offset + size - 1;
+	}
+}
